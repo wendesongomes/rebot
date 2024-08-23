da109bcac2d67b07e69db870d4a66f528cc6f1c3
wendesongomes <wendeson.gomes@live.com>
2024-08-23 00:21:02 -0300

    add dockefile for ffmpeg

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+# Use uma imagem base que já tenha Node.js e npm
+FROM node:18
+
+# Instale ffmpeg
+RUN apt-get update && \
+    apt-get install -y ffmpeg
+
+# Crie um diretório para o aplicativo
+WORKDIR /app
+
+# Copie o package.json e o package-lock.json (ou yarn.lock)
+COPY package*.json ./
+
+# Instale as dependências
+RUN npm install
+
+# Copie o restante do código do bot
+COPY . .
+
+# Compile o TypeScript para JavaScript
+RUN npm run build
+
+# Defina o comando para iniciar o bot
+CMD ["node", "server.js"]
--- src/services/commands/music.ts
+++ src/services/commands/music.ts
@@ -1,35 +1,50 @@
-import {
-  AudioPlayer,
-  AudioPlayerStatus,
-} from '@discordjs/voice'
-import { Message } from 'discord.js'
-import ytdl from '@distube/ytdl-core'
-import { next } from '../next'
-import { TQueue } from '../../server'
-import { toSmallText } from '../toSmallText'
+import { AudioPlayer, AudioPlayerStatus } from "@discordjs/voice";
+import { Message } from "discord.js";
+import ytdl from "@distube/ytdl-core";
+import { next } from "../next";
+import { TQueue } from "../../server";
+import { toSmallText } from "../toSmallText";
 
-export async function Music(message: Message, player: AudioPlayer, queue: TQueue) {
+export async function Music(
+  message: Message,
+  player: AudioPlayer,
+  queue: TQueue
+) {
   if (message.guildId && message.guild && message.member) {
-    if (message.content.startsWith('!rplay')) {
-      const link = message.content.split(' ')[1]
+    if (message.content.startsWith("!rplay")) {
+      const link = message.content.split(" ")[1];
 
       if (!link || !ytdl.validateURL(link)) {
-        return message.reply(toSmallText(`>>> Atenção\nVocê precisa fornecer um link do YouTube para o comando !rplay.`))
+        return message.reply(
+          toSmallText(
+            `>>> Atenção\nVocê precisa fornecer um link do YouTube para o comando !rplay.`
+          )
+        );
       }
 
       if (!message.member.voice.channelId) {
-        return message.reply(toSmallText(`>>> Atenção\nVocê precisa estar em um canal de voz para usar o comando !rplay.`))
+        return message.reply(
+          toSmallText(
+            `>>> Atenção\nVocê precisa estar em um canal de voz para usar o comando !rplay.`
+          )
+        );
       }
 
-      queue.push({ link, message })
+      queue.push({ link, message });
 
-      const title = (await ytdl.getInfo(link)).videoDetails.title
-      const queueLength = queue.length.toString().padStart(2, '0')
+      const title = (await ytdl.getInfo(link)).videoDetails.title;
+      const queueLength = queue.length.toString().padStart(2, "0");
+
+      console.log(title);
 
       if (player.state.status !== AudioPlayerStatus.Playing) {
-        next(player, queue)
+        next(player, queue);
       } else {
-        message.reply(toSmallText(`Musica Adicionada\n**${title}**\nMusicas Na Fila\u2003\u2003Posição Na Fila\n**${queueLength}**\u2003\u2003\u2003\u2003\u2003\u2003\u2003 **${queueLength}**`))
+        message.reply(
+          toSmallText(
+            `Musica Adicionada\n**${title}**\nMusicas Na Fila\u2003\u2003Posição Na Fila\n**${queueLength}**\u2003\u2003\u2003\u2003\u2003\u2003\u2003 **${queueLength}**`
+          )
+        );
       }
     }
   }
